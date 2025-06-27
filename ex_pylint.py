d82826690e19e251c2d2c2ecc81e717406fb3e05
Guilherme Jorge Hauck <78877899+Guihauck@users.noreply.github.com>
2025-06-27 11:16:16 -0300

    config: Starting Project With Git And Requirements.txt'

---
+++ ex_pylint.py
@@ -0,0 +1,10 @@
+'''Teste'''
+print("Ola Mundo")
+
+def minha_funcao():
+    '''Oi'''
+    print("Estou na minha funcao")
+
+#snake_case -> variáveis e funções
+#camelCase -> Não consta
+#PascalCase -> nomes de classes
