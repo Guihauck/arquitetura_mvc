40b9917281f29e8f15b88b58558794b94ecdac84
Guilherme Jorge Hauck <78877899+Guihauck@users.noreply.github.com>
2025-07-28 10:50:12 -0300

    feat: Implementing interfaces in controllers

--- src/controllers/person_creator_controller.py
+++ src/controllers/person_creator_controller.py
@@ -1,8 +1,9 @@
 from typing import Dict
 import re
 from src.models.sqlite.interfaces.people_repository import PeopleRepositoryInterface
+from .interfaces.person_creator_controller import PeopleCreatorControllerInterface
 
-class PeopleCreatorController:
+class PeopleCreatorController(PeopleCreatorControllerInterface):
     def __init__(self, people_repository: PeopleRepositoryInterface) -> None:
         self.__people_repository = people_repository
 
